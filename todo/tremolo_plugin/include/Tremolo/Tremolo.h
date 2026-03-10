a9fea8e04026948a269b63212e9d114431ebc433
jonah <jonah.gryfe@gmail.com>
2026-03-10 13:18:49 +0100

    Triangle waveform created buffered LFO change implemented Enum classes created and implemented for LFO shapes

--- todo/tremolo_plugin/include/Tremolo/Tremolo.h
+++ todo/tremolo_plugin/include/Tremolo/Tremolo.h
@@ -3,9 +3,14 @@
 namespace tremolo {
 class Tremolo {
 public:
+    enum class LfoWaveform : size_t{
+        sine = 0,
+        triangle = 1,
+    };
   Tremolo() {
-    lfo.setFrequency(5.f /* Hz */, true
-      );
+    for (auto& lfo : lfos) {
+      lfo.setFrequency(5.f /* Hz */, true);
+    }
   }
   void prepare(double sampleRate, int expectedMaxFramesPerBlock) {
     const juce::dsp::ProcessSpec processSpec{
@@ -13,18 +18,28 @@ public:
     .maximumBlockSize = static_cast<juce::uint32>(expectedMaxFramesPerBlock),
     .numChannels = 1u,
     };
-    lfo.prepare(processSpec);
+
+    for (auto& lfo : lfos){
+      lfo.prepare(processSpec);
+    }
+  }
+
+  void setLfoWaveform(LfoWaveform waveform){
+    jassert(waveform == LfoWaveform::sine || waveform == LfoWaveform::triangle);
+
+    lfoToSet = waveform;
   }
 
   void process(juce::AudioBuffer<float>& buffer) noexcept {
+    updateLfoWaveform();
     // for each frame
     for (const auto frameIndex : std::views::iota(0, buffer.getNumSamples())) {
       // generate the LFO value
-      const auto lfoValue = lfo.processSample(0.f);
+      const auto lfoValue = getNextLfoValue();
 
         // calculate the modulation value
       constexpr auto modulationDepth = 0.4f;
-      const auto modulationValue = modulationDepth * lfoValue + 1.f;
+      const auto modulationValue = modulationDepth * (lfoValue + 1.f);
 
 
 
@@ -43,11 +58,40 @@ public:
     }
   }
 
-  void reset() noexcept {
-    lfo.reset();
+
+
+    void reset() noexcept {
+    for (auto& lfo : lfos){
+      lfo.reset();
+    }
   }
 private:
   // You should put class members and private functions here
-  juce::dsp::Oscillator<float> lfo{ [](auto phase){ return std::sin(phase); }};
+
+    static float triangle(float phase){
+    const auto ft = phase / juce::MathConstants<float>::twoPi;
+    return 4.f * std::abs(ft - std::floor(ft + 0.5f)) - 1.f;
+  }
+
+
+    float getNextLfoValue()
+    { return lfos[juce::toUnderlyingType(currentLfo)].processSample(0.f); }
+
+    void updateLfoWaveform(){
+    if (currentLfo != lfoToSet)
+      currentLfo = lfoToSet;
+    }
+
+  //juce::dsp::Oscillator<float> lfo{ [](auto phase){ return std::sin(phase); }};
+
+  std::array<juce::dsp::Oscillator<float>, 2u> lfos{
+
+          juce::dsp::Oscillator<float>{[](auto phase) { return std::sin(phase); }},
+          juce::dsp::Oscillator<float>{triangle},
+  };
+
+
+  LfoWaveform currentLfo = LfoWaveform::sine;
+  LfoWaveform lfoToSet = currentLfo;
 };
 }  // namespace tremolo
